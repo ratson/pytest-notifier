ec57ca36929338d921bf23d1e1dc5c97bac035bb
Ratson <github@ratson.name>
2016-01-09 22:14:43 +0800

    Add libnotify support

--- pytest_notifier/notifier.py
+++ pytest_notifier/notifier.py
@@ -1,4 +1,11 @@
+import sys
 import subprocess
+
+
+def notify_via_libnotify(title, body):
+    subprocess.check_call([
+        'notify-send', title, body,
+    ])
 
 
 def notify_via_terminal_notifier(title, body):
@@ -18,10 +25,15 @@ def notify_via_apple_script(title, body):
 
 
 def notify(title, body):
-    for f in (
-        notify_via_terminal_notifier,
-        notify_via_apple_script,
-    ):
+    notify_functions = {
+        'darwin': (notify_via_terminal_notifier,
+                   notify_via_apple_script,
+                   notify_via_libnotify),
+        'linux': (notify_via_libnotify,),
+    }.get(sys.platform, (notify_via_libnotify,
+                         notify_via_terminal_notifier,
+                         notify_via_apple_script,))
+    for f in notify_functions:
         try:
             f(title, body)
         except OSError:
