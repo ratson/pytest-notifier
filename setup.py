9ca11ff0c19c6052e71418e4abf2659de4f9f040
Ratson <github@ratson.name>
2017-01-18 21:45:44 +0800

    Test against Python 3.6

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setuptools.setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 
     entry_points={
